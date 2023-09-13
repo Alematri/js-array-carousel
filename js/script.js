b00a1e3ad4e5dec47dff71582fbf7b1251f995fa
Alematri <alessandromatricardi@gmail.com>
2023-09-13 16:38:44 +0200

    [Milestones 1 to 3] complete

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const itemsWrapper = document.querySelector('.items-wrapper');
+const btnPrev = document.querySelector('.up');
+const btnNext = document.querySelector('.down');
+
+btnPrev.classList.add('hide');
+
+const images = [
+  'img/01.webp',
+  'img/02.webp',
+  'img/03.webp',
+  'img/04.webp',
+  'img/05.webp',
+];
+
+let counterImg=0
+
+for (let i=0; i< images.length; i++){
+  const image = images[i];
+  itemsWrapper.innerHTML += `<img src="${image}" class="item hide">`;
+}
+
+const itemsCollection = document.getElementsByClassName('item');
+
+itemsCollection[counterImg].classList.remove('hide');
+
+btnNext.addEventListener('click', function(){
+  itemsCollection[counterImg].classList.add('hide');
+
+  counterImg++;
+
+  itemsCollection[counterImg].classList.remove('hide');
+
+  btnPrev.classList.remove('hide');
+
+  if(counterImg === itemsCollection.length -1){
+  btnNext.classList.add('hide');
+  }
+});
+
+btnPrev.addEventListener('click', function() {
+  itemsCollection[counterImg].classList.add('hide');
+
+  counterImg--;
+
+  itemsCollection[counterImg].classList.remove('hide');
+
+  btnNext.classList.remove('hide');
+
+
+  if (counterImg === 0) btnPrev.classList.add('hide');
+  });
