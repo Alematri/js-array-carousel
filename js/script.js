f8917b818ea7c6e947fbca7506ccdb18afb69526
Alematri <alessandromatricardi@gmail.com>
2023-10-05 09:50:16 +0200

    [fixed] img path

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ const btnNext = document.querySelector('.down');
 const ThubsWrapper = document.querySelector('.thubs-wrapper');
 
 const images = [
-  '../img/01.webp',
-  '../img/02.webp',
-  '../img/03.webp',
-  '../img/04.webp',
-  '../img/05.webp',
-  '../img/06.webp',
-  '../img/07.webp',
-  '../img/08.webp',
-  '../img/09.webp',
-  '../img/10.webp',
-  '../img/11.webp',
-  '../img/12.webp',
+  './img/01.webp',
+  './img/02.webp',
+  './img/03.webp',
+  './img/04.webp',
+  './img/05.webp',
+  './img/06.webp',
+  './img/07.webp',
+  './img/08.webp',
+  './img/09.webp',
+  './img/10.webp',
+  './img/11.webp',
+  './img/12.webp',
 ];
 
 let counterImg=0
